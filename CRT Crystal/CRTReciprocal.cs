d9a205eba67c51974bd00e90640d6fb40192c15d
John K N Murphy <j.murpheous@gmail.com>
2024-02-13 18:29:20 +1300

    Preliminary to add CRT for Crystal Simulation

---
+++ CRT Crystal/CRTReciprocal.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+// A Udon model of the quantum reactions in a crystal based on William Duane's 1923 hypotheisis
+// This version aims to use a custom render texture to speed up the calculation of the set of reaction
+// Vectors and the beam angle of incidence.
+public class CRTReciprocal : UdonSharpBehaviour
+{
+    [SerializeField]
+    private CustomRenderTexture customRenderTexture;
+    void Start()
+    {
+        
+    }
+}
